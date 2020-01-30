dc0d02237834882e69db9441789232c3fb64cfa7
gianlucaparadise <gianlucaparadise@gmail.com>
2020-01-30 21:54:28 +0100

    FadeIn image from network

--- lib/widgets/video_thumbnail.dart
+++ lib/widgets/video_thumbnail.dart
@@ -1,5 +1,6 @@
 import 'package:cast_videos_flutter/models/video_descriptor.dart';
 import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class VideoThumbnail extends StatelessWidget {
   VideoThumbnail({this.video, this.imagePrefix});
@@ -14,11 +15,11 @@ class VideoThumbnail extends StatelessWidget {
     return AspectRatio(
       aspectRatio: 480.0 / 270.0,
       child: Hero(
-        // FIXME: this tag should be unique, but right now it isn't. An ID property is missing in the original json
+        // FIXME: this tag should be unique, but right now it isn't. An ID property is missing in the original json. Should I generate one?
         tag: '${video.title}',
-        child: Image.network(
-          imageUrl,
-          fit: BoxFit.cover,
+        child: FadeInImage.memoryNetwork(
+          placeholder: kTransparentImage,
+          image: imageUrl,
         ),
       ),
     );
