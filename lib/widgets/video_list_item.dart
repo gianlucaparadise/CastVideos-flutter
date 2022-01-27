449f01bc3139b25fdd5732405ca162cac73fd0d2
gianlucaparadise <gianlucaparadise@gmail.com>
2022-01-27 22:25:49 +0100

    More button in list

--- lib/widgets/video_list_item.dart
+++ lib/widgets/video_list_item.dart
@@ -1,31 +1,61 @@
+import 'package:cast_videos_flutter/cast/cast_manager.dart';
+import 'package:cast_videos_flutter/models/video_catalog.dart';
 import 'package:cast_videos_flutter/models/video_descriptor.dart';
+import 'package:cast_videos_flutter/routes/expanded_controls_route.dart';
 import 'package:cast_videos_flutter/widgets/video_heading.dart';
+import 'package:cast_videos_flutter/widgets/video_player_popup_menu_button.dart';
 import 'package:cast_videos_flutter/widgets/video_thumbnail.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class VideoListItem extends StatelessWidget {
   VideoListItem({required this.video});
 
   final VideoDescriptor video;
 
+  void _onPlayNowToCast(BuildContext context, CastManager castManager) {
+    final catalog = Provider.of<VideoCatalog?>(context, listen: false);
+    final categories = catalog?.categories;
+    if (categories == null || categories.isEmpty) {
+      debugPrint("missing catalog categories, cannot load media");
+      return;
+    }
+
+    final category = categories.first;
+    castManager.loadMedia(this.video, category, 0, true);
+
+    openExpandedControls(context);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          flex: 1,
-          child: VideoThumbnail(
-            video: video,
-          ),
-        ),
-        Container(width: 10), // this is a spacer
-        Expanded(
-          flex: 1,
-          child: VideoHeading(
-            video: video,
-          ),
-        )
-      ],
+    return Consumer<CastManager>(
+      builder: (context, castManager, child) {
+        final isCastConnected =
+            castManager.castConnectionState == CastConnectionState.CONNECTED;
+        final moreMenuButton = MorePlayerPopupMenuButton(
+          onPlayNow: () => _onPlayNowToCast(context, castManager),
+        );
+
+        return Row(
+          children: <Widget>[
+            Expanded(
+              flex: 1,
+              child: VideoThumbnail(
+                video: video,
+              ),
+            ),
+            Container(width: 10), // this is a spacer
+            Expanded(
+              flex: 1,
+              child: VideoHeading(
+                video: video,
+              ),
+            ),
+            isCastConnected ? moreMenuButton : SizedBox.shrink(),
+          ],
+        );
+      },
     );
   }
 }
--- lib/widgets/video_player_popup_menu_button.dart
+++ lib/widgets/video_player_popup_menu_button.dart
@@ -7,7 +7,7 @@ enum VideoPlayerPopupMenuOptions {
 }
 
 class VideoPlayerPopupMenuButton extends StatelessWidget {
-  VideoPlayerPopupMenuButton({
+  const VideoPlayerPopupMenuButton({
     this.onPlayNow,
     this.onAddToQueue,
   });
@@ -17,12 +17,52 @@ class VideoPlayerPopupMenuButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return PopupMenuButton<VideoPlayerPopupMenuOptions>(
-      child: Icon(
+    return _PlayQueuePopupMenuButton(
+      icon: Icon(
         Icons.play_circle_filled,
         color: Colors.white,
         size: 80,
       ),
+      onPlayNow: this.onPlayNow,
+      onAddToQueue: this.onAddToQueue,
+    );
+  }
+}
+
+class MorePlayerPopupMenuButton extends StatelessWidget {
+  const MorePlayerPopupMenuButton({
+    this.onPlayNow,
+    this.onAddToQueue,
+  });
+
+  final VoidCallback? onPlayNow;
+  final VoidCallback? onAddToQueue;
+
+  @override
+  Widget build(BuildContext context) {
+    return _PlayQueuePopupMenuButton(
+      icon: Icon(Icons.more_vert),
+      onPlayNow: this.onPlayNow,
+      onAddToQueue: this.onAddToQueue,
+    );
+  }
+}
+
+class _PlayQueuePopupMenuButton extends StatelessWidget {
+  const _PlayQueuePopupMenuButton({
+    required this.icon,
+    this.onPlayNow,
+    this.onAddToQueue,
+  });
+
+  final Widget icon;
+  final VoidCallback? onPlayNow;
+  final VoidCallback? onAddToQueue;
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<VideoPlayerPopupMenuOptions>(
+      child: this.icon,
       onSelected: (value) {
         switch (value) {
           case VideoPlayerPopupMenuOptions.PlayNow:
